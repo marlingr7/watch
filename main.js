f00def59aaccf5e239a0b47a5d370bb1c118df6f
marlingr7 <marlin.gu7@gmail.com>
2021-09-16 15:09:19 -0500

    format

--- main.js
+++ main.js
@@ -142,10 +142,26 @@ $("#watch").click(function (e) {
       "rgba(2,2,6,255)",
       "rgba(244,244,244,255)"
     );
-    
+
     //Circles start
-    drawCircle(ctx, xPosition, yPosition, 10, 0.5, "rgba(86,86,86,255)", "rgba(2,2,6,255)");
-    drawCircle(ctx, xPosition, yPosition, 7, 0.5, "rgba(86,86,86,255)", "rgba(2,2,6,255)");
+    drawCircle(
+      ctx,
+      xPosition,
+      yPosition,
+      10,
+      0.5,
+      "rgba(86,86,86,255)",
+      "rgba(2,2,6,255)"
+    );
+    drawCircle(
+      ctx,
+      xPosition,
+      yPosition,
+      7,
+      0.5,
+      "rgba(86,86,86,255)",
+      "rgba(2,2,6,255)"
+    );
     drawCircle(
       ctx,
       xPosition,
@@ -174,38 +190,37 @@ $("#watch").click(function (e) {
   }
 })();
 
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker
-      .register('./sw.js')
-      .then(() => { console.log('Service Worker Registered'); });
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker.register("./sw.js").then(() => {
+    console.log("Service Worker Registered");
+  });
 }
 
 // Code to handle install prompt on desktop
 
-
 let deferredPrompt;
-const addBtn = document.querySelector('.add-button');
+const addBtn = document.querySelector(".add-button");
 //addBtn.style.display = 'none';
 
-window.addEventListener('beforeinstallprompt', (e) => {
+window.addEventListener("beforeinstallprompt", (e) => {
   // Prevent Chrome 67 and earlier from automatically showing the prompt
   e.preventDefault();
   // Stash the event so it can be triggered later.
   deferredPrompt = e;
   // Update UI to notify the user they can add to home screen
-  addBtn.style.display = 'block';
+  addBtn.style.display = "block";
 
-  addBtn.addEventListener('click', () => {
+  addBtn.addEventListener("click", () => {
     // hide our user interface that shows our A2HS button
-    addBtn.style.display = 'none';
+    addBtn.style.display = "none";
     // Show the prompt
     deferredPrompt.prompt();
     // Wait for the user to respond to the prompt
     deferredPrompt.userChoice.then((choiceResult) => {
-      if (choiceResult.outcome === 'accepted') {
-        console.log('User accepted the A2HS prompt');
+      if (choiceResult.outcome === "accepted") {
+        console.log("User accepted the A2HS prompt");
       } else {
-        console.log('User dismissed the A2HS prompt');
+        console.log("User dismissed the A2HS prompt");
       }
       deferredPrompt = null;
     });
--- sw.js
+++ sw.js
@@ -1,27 +1,31 @@
-self.addEventListener('install', (e) => {
+self.addEventListener("install", (e) => {
   e.waitUntil(
-    caches.open('watch-store').then((cache) => cache.addAll([
-      './index.html',
-      './main.js',
-      './manifest.mf',
-      './audio/mickey_mouse_hey.mp3',
-      './css/style.css',
-      './css/fontello.css',
-      './font/fontello.eot',
-      './font/fontello.svg',
-      './font/fontello.ttf',
-      './font/fontello.woff',
-      './font/fontello.woff2',
-      './img/mickey_icon_192x192.png',
-      './img/watch.jpg',
-      './img/mickey.ico',
-    ])),
+    caches
+      .open("watch-store")
+      .then((cache) =>
+        cache.addAll([
+          "./index.html",
+          "./main.js",
+          "./manifest.mf",
+          "./audio/mickey_mouse_hey.mp3",
+          "./css/style.css",
+          "./css/fontello.css",
+          "./font/fontello.eot",
+          "./font/fontello.svg",
+          "./font/fontello.ttf",
+          "./font/fontello.woff",
+          "./font/fontello.woff2",
+          "./img/mickey_icon_192x192.png",
+          "./img/watch.jpg",
+          "./img/mickey.ico",
+        ])
+      )
   );
 });
 
-self.addEventListener('fetch', (e) => {
+self.addEventListener("fetch", (e) => {
   console.log(e.request.url);
   e.respondWith(
-    caches.match(e.request).then((response) => response || fetch(e.request)),
+    caches.match(e.request).then((response) => response || fetch(e.request))
   );
 });
